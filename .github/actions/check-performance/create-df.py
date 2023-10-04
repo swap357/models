c2e3d8f1512edb3f817aa09933071026662f550a
gera-aldama <111396864+gera-aldama@users.noreply.github.com>
2023-10-04 11:54:43 -0600

    Gda/step url (#1560)
    * Added step url to result table

--- .github/actions/check-performance/create-df.py
+++ .github/actions/check-performance/create-df.py
@@ -13,14 +13,15 @@ TOLERANCE = sys.argv[6]
 THRESHOLD = sys.argv[7]
 RESULT = sys.argv[8]
 PASS = sys.argv[9]
-OUTPUT_FOLDER = sys.argv[10]
+STEP_URL = sys.argv[10]
+OUTPUT_FOLDER = sys.argv[11]
 
 ############### Add parameters to table ##########################
 if os.path.isfile(f'{OUTPUT_FOLDER}/performances/result-table_{WORKLOAD}_{SCRIPT_NAME}.csv') == False:
-    result_df = pd.DataFrame(columns=['Framework', 'Workload', 'Script', 'Precision', 'Benchmark Type', 'Tolerance', 'Threshold', 'Result', 'Pass/Fail'])
-    result_df.loc[len(result_df.index)] = [FRAMEWORK, WORKLOAD, SCRIPT, PRECISION, BENCHMARK_TYPE, TOLERANCE, THRESHOLD, RESULT, PASS]
+    result_df = pd.DataFrame(columns=['Framework', 'Workload', 'Script', 'Precision', 'Benchmark Type', 'Tolerance', 'Threshold', 'Result', 'Pass/Fail', 'URL'])
+    result_df.loc[len(result_df.index)] = [FRAMEWORK, WORKLOAD, SCRIPT, PRECISION, BENCHMARK_TYPE, TOLERANCE, THRESHOLD, RESULT, PASS, STEP_URL]
     result_df.to_csv(f'{OUTPUT_FOLDER}/performances/result-table_{WORKLOAD}_{SCRIPT_NAME}.csv', index=False)
 else:
     result_df = pd.read_csv(f'{OUTPUT_FOLDER}/performances/result-table_{WORKLOAD}_{SCRIPT_NAME}.csv')
-    result_df.loc[len(result_df.index)] = [FRAMEWORK, WORKLOAD, SCRIPT, PRECISION, BENCHMARK_TYPE, TOLERANCE, THRESHOLD, RESULT, PASS]
+    result_df.loc[len(result_df.index)] = [FRAMEWORK, WORKLOAD, SCRIPT, PRECISION, BENCHMARK_TYPE, TOLERANCE, THRESHOLD, RESULT, PASS, STEP_URL]
     result_df.to_csv(f'{OUTPUT_FOLDER}/performances/result-table_{WORKLOAD}_{SCRIPT_NAME}.csv', index=False)
--- .github/utils/create-md-table.py
+++ .github/utils/create-md-table.py
@@ -3,27 +3,26 @@ import os
 import pandas as pd
 import json
 from datetime import date
-
 dataframes = {}
-
 today = date.today()
-
-GITHUB_URL = sys.argv[1]
-
 ############### Change csv files to dataframes ##########################
 for subdir, dirs, files in os.walk('./performances'):
     idx = 0
     for file in files:
         dataframes[f'df_{idx}'] = pd.read_csv(f'./performances/{file}')
         idx += 1
-
 ############ Concatenate dataframes and create the MD table ##############
 result_df = pd.concat(dataframes, axis=0).reset_index()
 result_df.set_index('Framework', inplace=True)
 result_df = result_df.drop(columns=['level_0', 'level_1'])
-result_markdown_table = result_df.to_markdown()
-with open('result_summary_table.md', 'w') as file:
-    file.write(result_markdown_table)
+workload_list = result_df['Workload'].values.tolist()
+workload_list = list(set(workload_list))
+result_tables = {}
+for workload in workload_list:
+    result_tables[workload] = result_df[result_df['Workload'] == workload]
+    result_markdown_table = result_tables[workload].to_markdown()
+    with open(f'result_summary_table_{workload}.md', 'w') as file:
+        file.write(result_markdown_table)
 
 ###################### Convert dataframe to json ##########################
 frameworks = []
@@ -37,7 +36,6 @@ results = []
 passes_fails = []
 results_dates = []
 urls = []
-
 result_df = result_df.reset_index()
 for index, row in result_df.iterrows():
     frameworks.append(row['Framework'])
@@ -50,8 +48,7 @@ for index, row in result_df.iterrows():
     results.append(row['Result'])
     passes_fails.append(row['Pass/Fail'])
     results_dates.append(today.strftime("%Y-%m-%d"))
-    urls.append(GITHUB_URL)
-
+    urls.append(row['URL'])
 json_content = {
     "framework": frameworks,
     "workload": workloads,
@@ -66,7 +63,5 @@ json_content = {
     "url": urls
 }
 result_json = json.dumps(json_content)
-print(result_json)
-
 with open("result_summary_table.json", "w") as outfile:
     outfile.write(result_json)
