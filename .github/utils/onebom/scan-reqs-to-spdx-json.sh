5b14c65da92c7765231de7dd80113db1513ac74b
WafaaT <wafaa.s.taie@intel.com>
2024-01-23 12:02:26 -0800

    remove GHA workflows

--- .github/utils/onebom/scan-reqs-to-spdx-json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in $(find . -name *requirements.txt) ; do stripped="${i:2:-17}" ; echo "${stripped}." ; trivy fs --format spdx-json --list-all-pkgs --scanners vuln -o "${stripped}/trivy-scan-spdx.json" "${stripped}" ; done
